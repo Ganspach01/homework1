a0f2c2a6b5a800619dced31b4851b4a6a34eafcf
Grace Anspach <123026604+Ganspach01@users.noreply.github.com>
2023-02-03 10:42:40 -0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,27 @@
+#Grace Anspach
+#Problem 1
+type("Rosewater")
+type(0.001)
+type(37)
+type(False)
+#Problem 2
+print("My","name","is","Grace")
+#this is an example using commas. the comma adds a space between each string
+print("My"+"name"+"is"+"Grace")
+#this is an example using plus signs. the plus side adds each of the strings together and makes them into one thing.
+#Problem 3
+#we can use int, float, str, and bool to convert arguments.
+int(5.25)
+float(8)
+str(5)
+bool(1)
+#Problem 4
+print("My name is Grace\nI am a humanistic studies major\nI have a minor in music\nI am in four classes this semester\none of these classes is a computer science class")
+#Problem 5
+flour=2
+milk=1
+egg=4
+oil=.4
+vanilla=0.012
+cake=2*flour+0.5*(milk-egg+vanilla)+oil**2
+print(cake)
